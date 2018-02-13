a769a30822fd149a7eef819d96f7fe85feed35a1
Jordan Castillo <jordantcastillo@gmail.com>
2018-02-13 15:12:47 -0800

    adding sshpass-1.06 to repo for redundancy reasons

--- Library/Formula/sshpass.rb
+++ Library/Formula/sshpass.rb
@@ -1,7 +1,7 @@
 require 'formula'
 
 class Sshpass < Formula
-  url 'http://sourceforge.net/projects/sshpass/files/sshpass/1.06/sshpass-1.06.tar.gz'
+  url 'https://github.com/jtcastillo/sshpass/raw/master/sshpass-1.06.tar.gz'
   homepage 'http://sourceforge.net/projects/sshpass'
   sha256 '7a07d3f7cca5c0b38ca811984ef8da536da32932d68c1a6cce33ec2462b930bf'
 
